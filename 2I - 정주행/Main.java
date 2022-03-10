00c9da608518a1e64bbae75e166c80bbc119b625
노찬욱 <nochan-ug@nochan-ug-ui-MacBookAir.local>
2022-03-11 00:33:16 +0900

    2I 정주행 complete

---
+++ 2I - 정주행/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+public class Main {
+	public static final Scanner scanner = new Scanner(System.in);
+
+	/**
+	 * 배열의 N개의 원소가 연속적인 정수 수열로 표현될 수 있는지 판단하는 함수
+	 * @param data
+	 * @param n
+	 * @return
+	 */
+	public static boolean isConsecutive(int[] data, int n)
+	{
+		Arrays.sort(data);
+		for (int i = 0; i < n -1; i++) 
+		{
+			if (data[i + 1] != data[i] + 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int n = scanner.nextInt();
+		int[] data = new int[n];
+		for(int i = 0 ; i < n ; i ++)
+		{
+			data[i] = scanner.nextInt();
+		}
+
+		boolean result = isConsecutive(data, n);
+
+		if(result)
+		{
+			System.out.println("YES");
+		}else{
+			System.out.println("NO");
+		}
+	}
+}
